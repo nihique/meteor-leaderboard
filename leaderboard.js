db5691c0d40a5d2170d20f1d26cc88f738c0b1f2
Martin Kovarik <kovarik.martin@gmail.com>
2012-04-20 23:27:32 +0200

    player => developers

--- leaderboard.js
+++ leaderboard.js
@@ -4,46 +4,46 @@ var App = {
   Helpers: {}
 };
 
-// Model::Players
-App.Models.Players = new Meteor.Collection("Players");
+// Model::Developers
+App.Models.Developers = new Meteor.Collection("Developers");
 
-App.Models.Players.init = function () {
-  if (App.Models.Players.find().count() > 0) return;
+App.Models.Developers.init = function () {
+  if (App.Models.Developers.find().count() > 0) return;
   var names = ["Martin", "Mirek", "Rasta", "Karel", "David", "Tomas"];
   _(names).each(function (name) {
-    App.Models.Players.insert({
+    App.Models.Developers.insert({
       name: name, 
-      score: App.getRandomScore()
+      score: App.Models.Developers.getRandomScore()
     });
   });
 };
 
-App.Models.Players.resetScore = function () {
-  App.Models.Players.find().forEach(function (player) {
-    App.Models.Players.update(
-      {_id: player._id}, 
-      {$set: {score: App.Models.Players.getRandomScore()}}
+App.Models.Developers.resetScore = function () {
+  App.Models.Developers.find().forEach(function (developer) {
+    App.Models.Developers.update(
+      {_id: developer._id}, 
+      {$set: {score: App.Models.Developers.getRandomScore()}}
     );
   });
 };
 
-App.Models.Players.getRandomScore = function () {
+App.Models.Developers.getRandomScore = function () {
   return Math.floor(Math.random()*50);
 };
 
-App.Models.Players.addPointsToSelectedPlayer = function (points) {
-  App.Models.Players.update(
-    App.Models.Players.getSelectedId(), 
+App.Models.Developers.addPointsToSelectedDeveloper = function (points) {
+  App.Models.Developers.update(
+    App.Models.Developers.getSelectedId(), 
     {$inc: {score: points}}
   );
 };
 
-App.Models.Players.getSelectedId = function () {
-  return Session.get("selected_player");
+App.Models.Developers.getSelectedId = function () {
+  return Session.get("selected_developer");
 };
 
-App.Models.Players.setSelectedId = function (id) {
-  return Session.set("selected_player", id);
+App.Models.Developers.setSelectedId = function (id) {
+  return Session.set("selected_developer", id);
 };
 
 // Helpers
@@ -57,49 +57,49 @@ if (Meteor.is_client) {
   App.Helpers.initSession();
 
   // Template's data
-  Template.leaderboard.players = function () {
+  Template.leaderboard.developers = function () {
     var sort = Session.get("sort") === "score"
       ? {score: -1, name: 1}
       : {name: 1, score: -1};
-    return App.Models.Players.find({}, {sort: sort});
+    return App.Models.Developers.find({}, {sort: sort});
   };
 
   Template.leaderboard.selected_name = function () {
-    var player = App.Models.Players.findOne(App.Models.Players.getSelectedId());
-    return player && player.name;
+    var developer = App.Models.Developers.findOne(App.Models.Developers.getSelectedId());
+    return developer && developer.name;
   };
 
   Template.leaderboard.sort = function () {
     return Session.get("sort");
   };
 
-  Template.player.selected = function () {
-    return App.Models.Players.getSelectedId() === this._id ? "selected" : '';
+  Template.developer.selected = function () {
+    return App.Models.Developers.getSelectedId() === this._id ? "selected" : '';
   };
 
   // Template's events
   Template.leaderboard.events = {
     'click #add1point': function () {
-      App.Models.Players.addPointsToSelectedPlayer(1);
+      App.Models.Developers.addPointsToSelectedDeveloper(1);
     },
     'click #add5points': function () {
-      App.Models.Players.addPointsToSelectedPlayer(1);
+      App.Models.Developers.addPointsToSelectedDeveloper(1);
     },
     'click #add10points': function () {
-      App.Models.Players.addPointsToSelectedPlayer(10);
+      App.Models.Developers.addPointsToSelectedDeveloper(10);
     },
     'click #sort': function () {
       var new_sort = Session.get("sort") !== 'name' ? 'name' : 'score';
       Session.set("sort", new_sort);
     },
     'click #reset': function() {
-      App.Models.Players.resetScore();
+      App.Models.Developers.resetScore();
     }
   };
 
-  Template.player.events = {
+  Template.developer.events = {
     'click': function () {
-      Session.set("selected_player", this._id);
+      Session.set("selected_developer", this._id);
     }
   };
 }
@@ -108,6 +108,6 @@ if (Meteor.is_client) {
 // Server only
 if (Meteor.is_server) {
   Meteor.startup(function () {
-    App.Models.Players.init();
+    App.Models.Developers.init();
   });
 }
